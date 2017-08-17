785f096244846a09be0f675b5861de0c432bd1b1
Serpil SAFA <serpil@multitek.com.tr>
2017-08-17 16:05:19 +0300

    Initial Commit

---
+++ GitTestProjesi/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitTestProjesi
+//
+//  Created by Adnan-iMac on 17.08.2017.
+//  Copyright © 2017 Serpil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
